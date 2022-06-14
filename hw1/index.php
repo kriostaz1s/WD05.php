ac2d94c829128f440b2bdea6050c9157bc136f42
kriostaz1s <jeniasamochick@gmail.com>
2022-06-14 21:52:43 +0300

    privet Igor

---
+++ hw1/index.php
@@ -0,0 +1,70 @@
+<?php
+
+// $res = rand(0, 100);
+// if (($res % 2) === 0 ){
+//    echo($res);
+// }
+// if(($res >= 0) && ($res<=10)){
+//     echo("1- 10");
+// }elseif(($res > 10) && ($res<=40)){
+//     echo("11-20");
+// }elseif(($res > 40) && ($res<=80)){
+//     echo("41-80");
+// }elseif(($res > 80) && ($res<=100)){
+//     echo("81-100");
+// }
+
+// // 1
+// $a = rand(0, 100); echo("a $a\n");
+// $b = rand(0, 100); echo("b $b\n");
+// $c = rand(0, 100); echo("c $c\n"); echo "<br>";
+// if ($a >= $b){
+//     if($a >= $c){
+//         echo("a $a");
+//     }else{
+//         echo("c $c");
+//     }
+// }else{
+//     if($b > $c){
+//         echo("b $b");
+//     }else{
+//         echo("c $c");
+//     }
+// }
+
+// //2
+// $a = rand(0, 100); echo("a $a\n");
+// $b = rand(0, 100); echo("b $b\n");
+// $c = rand(0, 100); echo("c $c\n"); echo "<br>";
+// if($a === $b || $b === $c || $a === $c ){
+// echo ("Ошибка");
+// }else{
+// if (($a > $b && $a < $c) || ($a > $c && $a < $b)){
+//     echo("a $a");
+// }
+// if (($b > $a && $b < $c) || ($b > $c && $a > $b)){
+//     echo("b $b");
+// }
+// if (($c > $b && $a > $c) || ($a < $c && $c < $b)){
+//     echo("c $c");
+// }
+// }
+
+// //3
+// $x = rand(-5, 5); echo("x $x\n");
+// $y = rand(-5, 5); echo("y $y\n"); echo "<br>";
+// if ($x ===0 || $y===0 ){
+//  echo("Лежит на координатной прямой");
+// }elseif($x > 0){
+//     if($y > 0){
+//         echo("1");
+//     }else{
+//         echo("4");
+//     }
+// }elseif($x < 0){
+//     if($y > 0){
+//         echo("2");
+//     }else{
+//         echo("3");
+//     }
+// }
